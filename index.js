7a4ad5567ea42767fc8a0a16ab0501595833c939
soil <sh.yoshida@live.jp>
2021-08-27 21:47:33 +0900

    async/await

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+const allowCrossDomain = function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, access_token"
+  );
+
+  // intercept OPTIONS method
+  if ("OPTIONS" === req.method) {
+    res.send(200);
+  } else {
+    next();
+  }
+};
+app.use(allowCrossDomain);
+
+app.get("/:id", (req, res) => {
+  console.log(`リクエスト受付ました。[id: ${req.params.id}]`);
+
+  setTimeout(function () {
+    console.log(`完了しました。[id: ${req.params.id}]`);
+    res.json({ id: req.params.id });
+  }, 3000);
 });
 
 app.listen(port, () => {
